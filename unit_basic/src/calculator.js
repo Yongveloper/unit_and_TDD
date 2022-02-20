911e18f350e9cbb0a2965c3552a04870bfc0879b
Yongveloper <aaa9015@naver.com>
2022-02-20 22:01:03 +0900

    [unit] 계산기 테스트 작성하기

---
+++ unit_basic/src/calculator.js
@@ -0,0 +1,31 @@
+class Caculator {
+  constructor() {
+    this.value = 0;
+  }
+
+  set(num) {
+    this.value = num;
+  }
+
+  clear() {
+    this.value = 0;
+  }
+
+  add(num) {
+    this.value += num;
+  }
+
+  subtract(num) {
+    this.value -= num;
+  }
+
+  multiply(num) {
+    this.value *= num;
+  }
+
+  divide(num) {
+    this.value /= num;
+  }
+}
+
+module.exports = Caculator;
